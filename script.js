eb73366cf62f3b6e4e5df9e12a93f2e3b50c7196
moses <leecmoses@gmail.com>
2022-07-08 13:18:48 -0400

    Date selection functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdownForm');
+const dateEl = document.getElementById('date-picker');
+
+// Set min date value
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);

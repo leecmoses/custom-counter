ea7afff4eecd0eb0f5862e77baf21796b5fe716d
moses <leecmoses@gmail.com>
2022-07-08 13:37:39 -0400

    Add event listener and write updateCountdown function

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set min date value
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Take Values from Form Input
+const updateCountdown = e => {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
